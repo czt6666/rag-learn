b3b52936fc37013e3bfd089691109514bf506138
czt <changzhitao12@126.com>
2025-08-13 01:23:36 +0800

    1

---
+++ embding/main.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLm-L6-v2')
+
+sentences = [
+    "The weather is lovely today.",
+    "It's so sunny outside.",
+    "He drove to the stadium."
+]
+
+embeddings = model.encode(sentences)
+print(embeddings.shape)
+
+# for i in embeddings:
+#     print(i)
+#     print("===========")
+
+similarities = model.similarity(embeddings, embeddings)
+print(similarities)
